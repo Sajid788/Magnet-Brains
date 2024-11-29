e667ec53cdc08c7ca660cee7dc1b9ee684d0b3e8
Sajid788 <mdsajidalam8340@gmail.com>
2024-11-29 22:22:47 +0530

    login and signup done

--- frontend/src/Authentication/Login.jsx
+++ frontend/src/Authentication/Login.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { setIsAdmin, setIsLogin, setUser } from "../Slices/AuthSlice";
-import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
+import { EyeOutlined, EyeInvisibleOutlined } from "@ant-design/icons";
 
 const Login = () => {
   // State for managing input values and loading/error states
@@ -91,46 +91,89 @@ const Login = () => {
   };
 
   return (
-    <div className="background_container_login">
-      <section>
-        <h3>Login</h3>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="input_container">
-            <label>Email</label>
-            <input
-              placeholder="Enter Your Email"
-              type="email"
-              name="email"
-              value={email}
-              onChange={handleChange}
+    <div className="background_container_login h-screen bg-slate-50 ">
+      <div className="flex justify-center gap-20  h-full items-center w-[90%] m-auto  ">
+        <div className=" max-w-md ">
+          <div className="flex items-center justify-center">
+            <img
+              src="https://cdni.iconscout.com/illustration/premium/thumb/task-management-6253704-5122443.png"
+              alt=""
+              className="w-40 h-24 object-contain"
             />
+            {/* <h2 className="text-4xl font-semibold text-[#2f59d5]">Task Manager</h2> */}
           </div>
-          <div className="input_container">
-            <label>Password</label>
-            <div className="password_container">
+          <div className="mt-12">
+            <h1 className="text-5xl font font-bold text-[#2f59d5] ">
+              {" "}
+              Manage Your Tasks with Effortlessly
+            </h1>
+          </div>
+        </div>
+
+        <div className="bg-white shadow shadow-slate-300 rounded-md p-8 w-96 border border-neutral-300">
+          <h3 className="text-3xl font-semibold text-[#2f59d5] text-center">
+            Welcome Back!
+          </h3>
+          <p className="text-neutral-500 text-center mt-1">
+            Keep all your credentials safe!
+          </p>
+          <form className="form mt-8 space-y-5" onSubmit={handleSubmit}>
+            <div className="input_container">
+              <label className="text-neutral-500">Email</label>
               <input
-                placeholder="Enter Your Password"
-                type={showPassword ? "text" : "password"}
-                name="password"
-                value={password}
+                placeholder="Enter Your Email"
+                type="email"
+                name="email"
+                value={email}
                 onChange={handleChange}
+                className="py-2 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
               />
-              <span 
-                className="password_toggle" 
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <EyeInvisibleOutlined /> : <EyeOutlined />}
-              </span>
             </div>
-          </div>
-          {error && <p className="error">{error}</p>}
-          <div className="btn_container">
-            <button className="btn" disabled={loading}>
-              {loading ? "Logging in ..." : "Login"}
-            </button>
-          </div>
-        </form>
-      </section>
+            <div className="">
+              <label className="text-neutral-500">Password</label>
+              <div className=" relative">
+                <input
+                  placeholder="Enter Your Password"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                  className="py-2 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
+                />
+                <span
+                  className=" absolute top-3.5 right-4"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeInvisibleOutlined className="text-neutral-700" />
+                  ) : (
+                    <EyeOutlined className="text-neutral-700" />
+                  )}
+                </span>
+              </div>
+            </div>
+            {error && <p className="error text-neutral-700">{error}</p>}
+            <div >
+              <button
+                className="btn mt-4 w-full py-2 bg-[#2f59d5] rounded-full "
+                disabled={loading}
+              >
+                {loading ? "Logging in ..." : "Login"}
+              </button>
+
+              <p className="text-neutral-600 mt-5">
+                Don’t have an account?{" "}
+                <span className="text-[#2f59d5] text-lg font-semibold hover:underline cursor-pointer"
+                onClick={() => navigate("/Signup")}
+                >
+                  Sign Up
+                </span>
+              </p>
+
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/Authentication/Signup.jsx
+++ frontend/src/Authentication/Signup.jsx
@@ -27,7 +27,6 @@ const Signup = () => {
     e.preventDefault();
     setFile({ ...file, loading: true });
 
-    // Validate input fields
     if (!name || !email || !title || !password) {
       setFile({ ...file, loading: false, error: "All fields are required" });
       toast.warning("All fields are required");
@@ -35,13 +34,12 @@ const Signup = () => {
     }
 
     try {
-      // Send signup request to the server
       const res = await axios.post("http://localhost:5000/api/user/register", {
         name,
         email,
         title,
         password,
-        isAdmin: isAdmin === "Yes", // Convert to boolean before sending
+        isAdmin: isAdmin === "Yes",
       });
 
       const data = res.data;
@@ -55,7 +53,6 @@ const Signup = () => {
         return;
       }
 
-      // Clear the form and notify success
       setFile({
         name: "",
         email: "",
@@ -67,8 +64,6 @@ const Signup = () => {
       });
 
       toast.success("Signup successful");
-
-      // Navigate to login page
       navigate("/login");
     } catch (error) {
       setFile({
@@ -81,78 +76,126 @@ const Signup = () => {
   };
 
   return (
-    <div className="background_container_signup">
-      <section>
-        <h3>Signup</h3>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="input_container">
-            <label>Name</label>
-            <input
-              placeholder="Enter Your Name"
-              type="text"
-              name="name"
-              value={name}
-              onChange={handleChange}
+    <div className="background_container_signup h-screen bg-slate-50">
+      <div className="flex justify-center gap-20  h-full items-center w-[90%] m-auto">
+        <div className="max-w-md">
+          <div className="flex items-center justify-center">
+            <img
+              src="https://cdni.iconscout.com/illustration/premium/thumb/task-management-6253704-5122443.png"
+              alt=""
+              className="w-40 h-24 object-contain"
             />
           </div>
-          <div className="input_container">
-            <label>Email</label>
-            <input
-              placeholder="Enter Your Email"
-              type="email"
-              name="email"
-              value={email}
-              onChange={handleChange}
-            />
+          <div className="mt-12">
+            <h1 className="text-5xl font font-bold text-[#2f59d5]">
+              {" "}
+              Join Us Today and Stay Organized!
+            </h1>
           </div>
-          <div className="input_container">
-            <label>Title</label>
-            <input
-              placeholder="Enter Your Title"
-              type="text"
-              name="title"
-              value={title}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="input_container">
-            <label>Password</label>
-            <div className="password_container">
+        </div>
+
+        <div className="bg-white shadow shadow-slate-300 rounded-md p-8 w-[35rem] border border-neutral-300">
+          <h3 className="text-3xl font-semibold text-[#2f59d5] text-center">
+            Create an Account
+          </h3>
+          <p className="text-neutral-500 text-center mt-1">
+            Let's get started!
+          </p>
+          <form className="form mt-4 space-y-5" onSubmit={handleSubmit}>
+            <div className="flex gap-2">
+            <div className="input_container">
+              <label className="text-neutral-500">Name</label>
               <input
-                placeholder="Enter Your Password"
-                type={showPassword ? "text" : "password"}
-                name="password"
-                value={password}
+                placeholder="Enter Your Name"
+                type="text"
+                name="name"
+                value={name}
                 onChange={handleChange}
+                className="py-2 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
               />
-              <span
-                className="password_toggle"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <EyeInvisibleOutlined /> : <EyeOutlined />}
-              </span>
             </div>
-          </div>
-          <div className="input_container">
-            <label>Admin</label>
-            <select
-              name="isAdmin"
-              value={isAdmin}
-              onChange={handleChange}
-              className="admin_dropdown"
-            >
-              <option value="No">No</option>
-              <option value="Yes">Yes</option>
-            </select>
-          </div>
-          {error && <p className="error">{error}</p>}
-          <div className="btn_container">
-            <button className="btn" disabled={loading}>
-              {loading ? "Signing up ..." : "Signup"}
-            </button>
-          </div>
-        </form>
-      </section>
+            <div className="input_container">
+              <label className="text-neutral-500">Email</label>
+              <input
+                placeholder="Enter Your Email"
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                className="py-2 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
+              />
+            </div>
+            </div>
+
+            <div className="input_container">
+              <label className="text-neutral-500">Title</label>
+              <input
+                placeholder="Enter Your Title"
+                type="text"
+                name="title"
+                value={title}
+                onChange={handleChange}
+                className="py-2 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
+              />
+            </div>
+            <div className="flex gap-2">
+            <div className="w-full">
+              <label className="text-neutral-500">Password</label>
+              <div className="relative">
+                <input
+                  placeholder="Enter Your Password"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                  className="py-2 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
+                />
+                <span
+                  className="absolute top-3.5 right-4"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeInvisibleOutlined className="text-neutral-700" />
+                  ) : (
+                    <EyeOutlined className="text-neutral-700" />
+                  )}
+                </span>
+              </div>
+            </div>
+            <div className="w-full">
+              <label className="text-neutral-500">Admin</label>
+              <select
+                name="isAdmin"
+                value={isAdmin}
+                onChange={handleChange}
+                className="py-2.5 w-full px-4 rounded-full text-neutral-800 border-2 border-[#2f59d5] mt-1 focus:outline-none"
+              >
+                <option value="No">No</option>
+                <option value="Yes">Yes</option>
+              </select>
+            </div>
+            </div>
+            {error && <p className="error text-neutral-700">{error}</p>}
+            <div>
+              <button
+                className="btn mt-4 w-full py-2 bg-[#2f59d5] rounded-full"
+                disabled={loading}
+              >
+                {loading ? "Signing up ..." : "Signup"}
+              </button>
+              <p className="text-neutral-600 mt-5">
+                Already have an account?{" "}
+                <span
+                  className="text-[#2f59d5] text-lg font-semibold hover:underline cursor-pointer"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </span>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
